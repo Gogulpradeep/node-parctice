d842d98b9387f73c25adfa88d0b6baae1851a1b9
gogulpradeep <mgogulpradeep@gmail.com>
2025-08-03 20:19:14 +0530

    updates

---
+++ math.js
@@ -0,0 +1,7 @@
+const math = require('./op');
+
+console.log(math.add(10,4));
+console.log(math.sub(12,4));
+console.log(math.mul(23,8));
+console.log(math.div(30,9));
+
--- op.js
+++ op.js
@@ -8,9 +8,19 @@
 //console.log(os.version());
 //console.log(os.homedir());
 
-const path = require("path");
+//const path = require("path");
 
-console.log(path.dirname(__filename));
-console.log(path.basename(__filename));
-console.log(path.extname(__filename));
+//console.log(path.dirname(__filename));
+//console.log(path.basename(__filename));
+//console.log(path.extname(__filename));
+//console.log(path.parse(__filename));
+
+const add =(a,b) => a+b;
+const sub =(a,b)=> a-b;
+const mul = (a,b)=> a*b;
+const div = (a,b)=> a/b;
+
+module.exports = {add,sub,mul,div};
+
+
 
