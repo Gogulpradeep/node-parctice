d88a0ef3c1685c2cef7abb1dbf93a7344365d8b5
gogulpradeep <mgogulpradeep@gmail.com>
2025-08-03 22:31:34 +0530

    updates

--- op.js
+++ op.js
@@ -15,12 +15,24 @@
 //console.log(path.extname(__filename));
 //console.log(path.parse(__filename));
 
-const add =(a,b) => a+b;
-const sub =(a,b)=> a-b;
-const mul = (a,b)=> a*b;
-const div = (a,b)=> a/b;
+//const add =(a,b) => a+b;
+//const sub =(a,b)=> a-b;
+//const mul = (a,b)=> a*b;
+//const div = (a,b)=> a/b;
 
-module.exports = {add,sub,mul,div};
+//module.exports = {add,sub,mul,div};
+
+const fs = require('fs');
+const  path = require('path');
+
+fs.readFile(path.join(__dirname,'files','start.txt'),'utf-8', (error,data)=>{
+    if (error){
+        throw error;
+
+    }else{
+        console.log(data);
+    }
+} );
 
 
 
