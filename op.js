1e88b17a83c5d28a1000ded2a33a9c2c1bec3db8
gogulpradeep <mgogulpradeep@gmail.com>
2025-08-03 11:28:45 +0530

    upadtes

--- op.js
+++ op.js
@@ -1,1 +1,9 @@
-console.log(global);
+//console.log(global);
+const os = require('os');
+
+//console.log(__filename);
+//console.log(__dirname);
+//console.log("____");
+//console.log(os.type());
+//console.log(os.version());
+//console.log(os.homedir());
