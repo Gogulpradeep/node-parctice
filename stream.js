abc14bc6ff6e633b5e18412806bc93370fc100f1
gogulpradeep <mgogulpradeep@gmail.com>
2025-08-04 22:34:40 +0530

    updates

---
+++ stream.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+const rs= fs.createReadStream(path.join('files','bigtext.txt'),{encoding:'utf-8'});
+const wr = fs.createWriteStream(path.join('files','bigfiles.txt'));
+
+rs.on('data',(dataChunk)=>{
+wr.write(dataChunk)
+
+})
