74d2136e4426ae104abfa25dd63244bd2389374a
gogulpradeep <mgogulpradeep@gmail.com>
2025-08-06 18:48:32 +0530

    upadtes

--- emit.js
+++ emit.js
@@ -1,11 +1,22 @@
-const EventEmitter = require('events');
-const  Emitter = new EventEmitter();
+//const EventEmitter = require('events');
+//const  Emitter = new EventEmitter();
 
-Emitter.on('eventname',(data)=>{
-    console.log('Event tiggered with',data);
+//Emitter.on('eventname',(data)=>{
+    //console.log('Event tiggered with',data);
+//});
+
+//Emitter.emit('eventname','hello');
+
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+
+emitter.on ('start',(data)=>{
+    console.log('process completed');
+});
+emitter.on('data',(info)=>{
+    console.log(`Received completed:${info}`);
 })
 
+emitter.emit('start');
+emitter.emit('data', ' node js modules powerful');
 
-
-
-Emitter.emit('eventname','hello');
--- n.js
+++ n.js
@@ -1,1 +1,19 @@
-console.log('gogul pradeep');
+const { error } = require('console');
+const http = require('http');
+const PORT = 3000;
+
+     const sever =  http.createServer((req,res)=>{
+        res.writeHead(200,{'content-type':"text/plain"});
+        console.log('hello sever world!');
+        res.end();
+
+ });
+
+
+ sever.listen(PORT,(error)=>{
+    if(error){
+        console.log(`something is wrong`,PORT);
+    }else{
+        console.log(`sever is running on http://localhost:3000`);
+    }
+ });
